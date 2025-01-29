1ffcba809d888bd7dcab74c119d5702660dd72fd
Marcos Medeiros <marcos.mb.mb@outlook.com>
2025-01-28 22:20:13 -0300

    finally

--- bot/telegram/server.py
+++ bot/telegram/server.py
@@ -1,6 +1,5 @@
 from flask import Flask, request
 from handlers import *
-import json
 
 app = Flask(__name__) ## Instancia um server
 
@@ -13,17 +12,15 @@ handlers = { #Comandos executados pelo BOT
 @app.route("/webhook", methods=['POST']) #endpoint de comunicação
 def webhook():
     update = request.get_json()
-
-    # Log completo da atualização recebida
-    print(json.dumps(update, indent=4))
     if 'message' in update:
-        id = update['message']['chat']['id']
-        text = update['message']['text']
-        list = text.split()
-        if len(list) == 1: 
-            handlers[list[0]].response(id)
-        else:
-            handlers[list[0]].response(id, " ".join(list[1:]))
+        if 'text' in update['message'] and update['message']['text'].startswith("/"):
+            id = update['message']['chat']['id']
+            text = update['message']['text']
+            list = text.split()
+            if len(list) == 1: 
+                handlers[list[0]].response(id)
+            else:
+                handlers[list[0]].response(id, " ".join(list[1:]))
     return "OK", 200
        
 if __name__ == '__main__':
